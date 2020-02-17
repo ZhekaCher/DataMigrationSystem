9fd07da43baec44b3737c6114f8357d5164a6bdb
Yevgeniy Cherdantsev <zhekacher20.05@gmail.com>
2020-02-17 17:34:24 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataMigrationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
