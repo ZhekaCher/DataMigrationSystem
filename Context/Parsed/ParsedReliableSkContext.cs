7ec2777236b676d333de8ae3f2fa180098e14632
yerss <abdirazakyersin@gmail.com>
2020-02-24 18:19:16 +0600

    Reliable SK's service and contexts

---
+++ Context/Parsed/ParsedReliableSkContext.cs
@@ -0,0 +1,20 @@
+﻿using DataMigrationSystem.Models.Parsed;
+using DataMigrationSystem.Models.Web.Avroradata;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Parsed
+{
+    public class ParsedReliableSkContext:DbContext
+    {
+        public ParsedReliableSkContext(DbContextOptions<ParsedReliableSkContext> options)
+            :base(options){}
+        public ParsedReliableSkContext(){}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("");
+        }
+        
+        public DbSet<ReliableSkDto> ReliableSkDtos { get; set; }
+    }
+}
---
+++ Services/ReliableSkMigrationService.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DataMigrationSystem.Context.Parsed;
+using DataMigrationSystem.Context.Web.Avroradata;
+using DataMigrationSystem.Models.Web.Avroradata;
+using Microsoft.EntityFrameworkCore;
+using NLog;
+
+namespace DataMigrationSystem.Services
+{
+    public class ReliableSkMigrationService : MigrationService
+    {
+        private readonly WebReliableSkContext _webReliableSkContext;
+        private readonly ParsedReliableSkContext _parsedReliableSkContext;
+
+        public ReliableSkMigrationService(int numOfThreats = 1)
+        {
+            NumOfThreads = numOfThreats;
+            _webReliableSkContext = new WebReliableSkContext();
+            _parsedReliableSkContext = new ParsedReliableSkContext();
+        }
+
+        protected override Logger InitializeLogger()
+        {
+            return LogManager.GetCurrentClassLogger();
+        }
+
+        public override  async Task StartMigratingAsync()
+        {
+            var reliableSkDtos = _parsedReliableSkContext.ReliableSkDtos
+                .Select(x => new ReliableSk
+                {
+                    Reason = x.Reason,
+                    AddingDate = x.AddingDate,
+                    RelevanceDate = x.RelevanceDate,
+                    Biin = x.Bin
+                });
+            foreach (var reliableSkDto in reliableSkDtos)
+            {
+                await _webReliableSkContext.ReliableSks.Upsert(reliableSkDto).On(x => x.Biin).RunAsync();
+            }
+        }
+    }
+}
