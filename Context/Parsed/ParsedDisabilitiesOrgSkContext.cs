9b3f14a48b823273be18957cc163227857ebec87
capomafiosoken <capomafiosoken@gmail.com>
2020-02-26 17:08:38 +0600

    Disabilities Context Fixed

--- Context/Parsed/ParsedDisabilitiesOrgSkContext.cs
+++ Context/Parsed/ParsedDisabilitiesOrgSkContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataMigrationSystem.Context.Parsed
 {
-    public class ParsedDisabilitiesOrgSkContext : DbContext
+    public class ParsedDisabilitiesOrgSkContext : ParsedContext
     {
         public DbSet<DisabilitiesOrgSkDto> DisabilitiesOrgSkDtos { get; set; }
     }
