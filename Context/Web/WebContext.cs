5aa31567afbe5f90ab48f470163daed586e084e2
capomafiosoken <capomafiosoken@gmail.com>
2020-02-26 10:38:46 +0600

    Db config upd

--- Context/Web/WebContext.cs
+++ Context/Web/WebContext.cs
@@ -17,7 +17,7 @@ namespace DataMigrationSystem.Context.Web
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(
-                "Server = 192.168.1.158; Database = avroradata; Port=5432; User ID = administrator; Password = Z4P6PjEHnJ5nPT; Search Path = avroradata; Integrated Security=true; Pooling=true;");
+                "Server = 192.168.1.158; Database = avroradata; Port=5432; User ID = data_migrator; Password = Z4P6PjEHnJ5nPT; Search Path = avroradata; Integrated Security=true; Pooling=true;");
         }
     }
 }
