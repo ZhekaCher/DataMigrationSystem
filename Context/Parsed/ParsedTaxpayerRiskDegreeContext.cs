776622078002fd096776b429ada4f5556c86f52c
yerss <abdirazakyersin@gmail.com>
2020-03-16 14:18:05 +0600

    Taxpayer Risk Degree MS,Context, Models

---
+++ Context/Parsed/ParsedTaxpayerRiskDegreeContext.cs
@@ -0,0 +1,12 @@
+﻿using DataMigrationSystem.Context.Web;
+using DataMigrationSystem.Models.Parsed;
+using DataMigrationSystem.Models.Web.Avroradata;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Parsed
+{
+    public class ParsedTaxpayerRiskDegreeContext:WebContext
+    {
+        public DbSet<TaxpayerRiskDegreeDto> TaxpayerRiskDegreeDtos { get; set; }
+    }
+}
---
+++ Context/Web/Avroradata/WebTaxpayerRiskDegreeContext.cs
@@ -0,0 +1,10 @@
+﻿using DataMigrationSystem.Models.Web.Avroradata;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Web.Avroradata
+{
+    public class WebTaxpayerRiskDegreeContext:WebContext
+    {
+        public DbSet<TaxpayerRiskDegree> TaxpayerRiskDegrees { get; set; }
+    }
+}
