14c9cd4c8449fe2a952bba0831ad1101b982f647
Аманжол Жасулан <z.amanzhol@wisk.kz>
2020-08-12 14:49:15 +0600

    add GosReesterMigreationService

---
+++ Models/Web/Avroradata/GosReester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataMigrationSystem.Models.Web.Avroradata
+{
+    [Table("gos_reester")]
+    public class GosReester
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        
+        [Column("reg_num")]
+        public string RegNum { get; set; }
+        
+        [Column("reg_date")]
+        public DateTime? RegDate { get; set; }
+        
+        [Column("status")]
+        public string Status { get; set; }
+        
+        [Column("owner")]
+        public string Owner { get; set; }
+        
+        [Column("name")]
+        public string? Name { get; set; }
+        
+        [Column("mktu_text")]
+        public string MktuText { get; set; }
+        
+        [Column("validation_date")]
+        public DateTime? ValidationDate { get; set; }
+        
+        [Column("newsletter_num")]
+        public string NewsletterNum { get; set; }
+        
+        [Column("newsletter_date")]
+        public DateTime? NewsletterDate { get; set; }
+
+        [Column("relevance_date")] public DateTime RelevanceDate { get; set; } = DateTime.Now;
+
+
+    }
+}
---
+++ Services/GosReesterMigrationService.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+using DataMigrationSystem.Context.Parsed;
+using DataMigrationSystem.Context.Web.Avroradata;
+using DataMigrationSystem.Models.Web.Avroradata;
+using Microsoft.EntityFrameworkCore;
+using NLog;
+
+namespace DataMigrationSystem.Services
+{
+    public class GosReesterMigrationService : MigrationService
+    {
+        private readonly WebGosRessterContext _webGosRessterContext;
+        private readonly ParsedGosReesterContext _parsedGosReesterContext;
+        private readonly object _forLock;
+        private int _counter;
+        
+        public GosReesterMigrationService(int numOfThreads = 1)
+        {
+            _webGosRessterContext = new WebGosRessterContext();
+            _parsedGosReesterContext = new ParsedGosReesterContext();
+            NumOfThreads = numOfThreads;
+            _forLock = new object();
+        }
+        protected override Logger InitializeLogger()
+        {
+            return LogManager.GetCurrentClassLogger();
+        }
+        public override async Task StartMigratingAsync()
+        {
+            await Migrate();
+            var gosReesterContext = new ParsedGosReesterContext();
+            await gosReesterContext.Database.ExecuteSqlRawAsync(
+                "truncate avroradata.gos_reester restart identity;");
+
+        }
+
+        private async Task Migrate()
+        {
+            var gosReesterDtos = _parsedGosReesterContext.GosReesterDtos;
+            await foreach (var gosReesterDto in gosReesterDtos)
+            {
+                var gosRessters = new GosReester
+                {
+                    RegNum = gosReesterDto.RegNum,
+                    RegDate = gosReesterDto.RegDate,
+                    Status = gosReesterDto.Status,
+                    Owner =gosReesterDto.Owner,
+                    Name = gosReesterDto.Name,
+                    MktuText = gosReesterDto.MktuText,
+                    ValidationDate = gosReesterDto.ValidationDate,
+                    NewsletterNum = gosReesterDto.NewsletterNum,
+                    NewsletterDate = gosReesterDto.NewsletterDate,
+                    RelevanceDate = gosReesterDto.RelevanceDate
+                }; 
+                await _webGosRessterContext.GosReesters.Upsert(gosRessters).On(X => X.RegNum).RunAsync();
+                lock (_forLock)
+                {
+                    Logger.Trace(_counter--);
+                }
+            }
+        }
+    }
+}
