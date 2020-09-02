8bf477cf446bf64fcaa4a9660cc34fa5cb7ae7c4
aibekzh <aibek_zhumash@mail.ru>
2020-09-02 13:58:51 +0600

    AD-676: EtsTenderMigration added

---
+++ Context/Parsed/ParsedEtsTenderContext.cs
@@ -0,0 +1,26 @@
+﻿using DataMigrationSystem.Models.Parsed;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Parsed
+{
+    public class ParsedEtsTenderContext: ParsedContext
+    {
+        public DbSet<AnnouncementEtsTenderDto> AnnouncementEtsTenderDtos { get; set; }
+        public DbSet<LotEtsTenderDto> LotEtsTenderDtos { get; set; }
+        public DbSet<PurchasingPositionsEtsTenderDto> PurchasingPositionsEtsTenderDtos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnnouncementEtsTenderDto>()
+                .HasMany(x => x.Lots)
+                .WithOne()
+                .HasForeignKey(x => x.SourceCode)
+                .HasPrincipalKey(x => x.SourceCode);
+            modelBuilder.Entity<AnnouncementEtsTenderDto>()
+                .HasMany(x => x.Positions)
+                .WithOne()
+                .HasForeignKey(x => x.SourceCode)
+                .HasPrincipalKey(x => x.SourceCode);
+        }
+    }
+}
