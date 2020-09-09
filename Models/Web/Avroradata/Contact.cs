f85473b1d6e705da65f12e2950e173a20ad3c342
aibekzh <aibek_zhumash@mail.ru>
2020-09-09 10:50:29 +0600

    contacts migration from tenders removed

--- Models/Web/Avroradata/Contact.cs
+++ Models/Web/Avroradata/Contact.cs
@@ -5,17 +5,6 @@ using System.Data;
 
 namespace DataMigrationSystem.Models.Web.Avroradata
 {
-    [Table("contacts")]
-    public class Contact
-    {
-        //[Key][Column("id")] public long Id { get; set; }
-        [Column("bin")] public long? Bin { get; set; }
-        [Column("email")] public string Email { get; set; }
-        [Column("website")] public string Website { get; set; }
-        [Column("telephone")] public string Telephone { get; set; }
-        [Column("source")] public string Source { get; set; }
-        [Column("relevance_date")] public DateTime RelevanceDate { get; set; }
-    }
     [Table("contacts_telephone")]
     public class ContactTelephone
     {
