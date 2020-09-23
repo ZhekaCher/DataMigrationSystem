bd143f2a98872c1f4e35fe2d831aba6a248451ff
aibekzh <aibek_zhumash@mail.ru>
2020-09-23 17:19:12 +0600

    gosreestr capitals  migration added

---
+++ Models/Parsed/Avroradata/GosreestrBinDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataMigrationSystem.Models.Parsed.Avroradata
+{
+    [Table("gosreestr_bins")]
+        public class GosreestrBinDto
+        {
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Column("id")]
+            public long Id { get; set; }
+
+            [Column("bin")] public long Bin { get; set; }
+            [Column("gov_participation")] public double? GovParticipation { get; set; }
+            public GosreestrCapitalDto GosreestrCapitalDto { get; set; }
+        }
+}
---
+++ Models/Web/Avroradata/GosreestrCapital.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataMigrationSystem.Models.Web.Avroradata
+{
+    [Table("gosreestr_authorized_capitals")]
+    public class GosreestrCapital
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("bin")] public long Bin { get; set; }
+        [Column("capital")] public string Capital { get; set; }
+        [Column("gov_contribution")] public string GovContribution { get; set; }
+        [Column("gov_participation")] public string GovParticipation { get; set; }
+        [Column("gov_package")] public string GovPackage { get; set; }
+        [Column("registrar")] public string Registrar { get; set; }
+        [Column("free_contribution")] public string FreeContribution { get; set; }
+        [Column("free_shares")] public string FreeShares { get; set; }
+        [Column("shares_encumbered")] public string SharesEncumbered { get; set; }
+    }
+}
