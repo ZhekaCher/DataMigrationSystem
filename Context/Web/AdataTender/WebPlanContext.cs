48766857f522aa6862579de1aeeaafda28e8cc48
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-09-29 09:19:42 +0600

    Updated Plan Migration

---
+++ Context/Web/AdataTender/WebPlanContext.cs
@@ -0,0 +1,10 @@
+﻿using DataMigrationSystem.Models.Web.AdataTender;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Web.AdataTender
+{
+    public partial class WebPlanContext:WebAdataTenderContext
+    {
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
