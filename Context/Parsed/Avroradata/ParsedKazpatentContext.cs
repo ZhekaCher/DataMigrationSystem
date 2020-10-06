9ca0235d3e5abe825deab0ee2230c88b3137030e
aibekzh <aibek_zhumash@mail.ru>
2020-10-06 10:44:24 +0600

    kazpatent migration added

---
+++ Context/Parsed/Avroradata/ParsedKazpatentContext.cs
@@ -0,0 +1,21 @@
+﻿using DataMigrationSystem.Models.Parsed.Avroradata;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Parsed.Avroradata
+{
+    public class ParsedKazpatentContext : ParsedAvroradataContext
+    {
+        public DbSet<KazpatentDto> KazpatentDtos { get; set; }
+        public DbSet<KazPatentOwnerDto> KazPatentOwnerDtos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<KazpatentDto>()
+                .HasMany(x => x.KazPatentOwnerDtos)
+                .WithOne()
+                .HasPrincipalKey(x => x.Id)
+                .HasForeignKey(x => x.PatentId);
+        }
+    }
+}
