c4142c26d633ed743ec3542aef4f3017956cc4a1
a.asylzhan <asylzhan.agataeev@gmail.com>
2020-10-09 17:42:45 +0600

    AD-902 National Bank Tender

---
+++ Context/Parsed/Avroradata/ParsedNationalBankTenderContext.cs
@@ -0,0 +1,20 @@
+﻿using DataMigrationSystem.Models.Parsed.Avroradata;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMigrationSystem.Context.Parsed.Avroradata
+{
+    public class ParsedNationalBankTenderContext : ParsedAvroradataContext
+    {
+        public DbSet<NationalBankTenderDto> NationalBankAdvert { get; set; }
+        public DbSet<NationalBankTenderLotDto> Lots { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NationalBankTenderDto>()
+                .HasMany(x => x.Lots)
+                .WithOne()
+                .HasForeignKey(x => x.AdvertId)
+                .HasPrincipalKey(x => x.AdvertId);
+        }
+    }
+}
