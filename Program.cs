d4812cd63abd9ef7a2728327b624950c3c176537
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-11-02 16:07:11 +0600

    Remove redundant method

--- Program.cs
+++ Program.cs
@@ -105,17 +105,5 @@ namespace DataMigrationSystem
                 throw;
             }
         }
-        private static string GetLocalIpAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            throw new Exception("No network adapters with an IPv4 address in the system!");
-        }
     }
 }
